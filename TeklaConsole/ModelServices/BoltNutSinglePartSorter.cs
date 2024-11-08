707c1356c3f875d266a0bacdcd56a45f213db790
Roman Urmancheev <matricarin@gmail.com>
2024-11-08 08:34:46 +0500

    new macros was added

--- TeklaConsole/ModelServices/BoltNutSinglePartSorter.cs
+++ TeklaConsole/ModelServices/BoltNutSinglePartSorter.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using TSM = Tekla.Structures.Model;
+using TMUI = Tekla.Structures.Model.UI;
+using TSMO = Tekla.Structures.Model.Operations;
 
 namespace TeklaConsole.ModelServices
 {
@@ -6,7 +10,74 @@ namespace TeklaConsole.ModelServices
     {
         public void Run()
         {
-            throw new NotImplementedException();
+            TSM.Model model = new TSM.Model();
+
+            if (model.GetConnectionStatus())
+                throw new Exception("I lost connection with Tekla.");
+
+            ArrayList boltsWithOneNutCollection = new ArrayList();
+
+            TMUI.ModelObjectSelector selector = new TMUI.ModelObjectSelector();
+            TSM.ModelObjectEnumerator modelObjectsEnumerator = selector.GetSelectedObjects();
+
+            if (modelObjectsEnumerator.GetSize() == 0)
+            {
+                TSMO.Operation.DisplayPrompt("You don't select any part.");
+                return;
+            }
+
+            while (modelObjectsEnumerator.MoveNext())
+            {
+                if (modelObjectsEnumerator.Current is TSM.Part part)
+                {
+                    TSM.ModelObjectEnumerator boltsEnumerator = part.GetBolts();
+                    while (boltsEnumerator.MoveNext())
+                    {
+                        if (boltsEnumerator.Current is TSM.BoltGroup boltGroup)
+                        {
+                            if (boltGroup.Nut1 == true && boltGroup.Nut2 == false)
+                            {
+                                boltsWithOneNutCollection.Add(boltGroup);
+                            }
+                        }
+                    }
+                }
+            }
+
+            ArrayList allBoltedParts = new ArrayList();
+
+            foreach (object obj in boltsWithOneNutCollection)
+            {
+                if (obj is TSM.BoltGroup bolt)
+                {
+                    allBoltedParts.Add(bolt.PartToBoltTo);
+                    allBoltedParts.Add(bolt.PartToBeBolted);
+                    allBoltedParts.AddRange(bolt.OtherPartsToBolt);
+                }
+            }
+
+            ArrayList result = new ArrayList();
+
+            foreach (object obj in allBoltedParts)
+            {
+                if (obj is TSM.Part part)
+                {
+                    TSM.Assembly assembly = part.GetAssembly();
+                    int secondariesPartsCount = assembly.GetSecondaries().Count;
+                    if (secondariesPartsCount == 0)
+                        result.Add(assembly);
+                }
+            }
+
+            if (result.Count == 0)
+            {
+                TSMO.Operation.DisplayPrompt("I don't find single-part assemblies.");
+            }
+            else
+            {
+                selector.Select(result);
+                TSMO.Operation.DisplayPrompt($"I find {result.Count} single-part assemblies.");
+            }
         }
     }
 }
